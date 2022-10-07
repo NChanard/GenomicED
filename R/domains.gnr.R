af66f310f3205f94316db75015f684932fb747db
NChanard <nchanard@univ-tlse3.fr>
2022-10-07 14:42:04 +0200

    Add interactions.gni data

--- R/domains.gnr.R
+++ R/domains.gnr.R
@@ -1,6 +1,6 @@
 #' Drosophila Melanogaster Tads on chromosome 2R and 2L (F.Ramirez 2018, https://doi.org/10.1038/s41467-017-02525-w)
 #'
-#' Data from a CHip Seq experiment
+#' Data from a HiC Seq experiment
 #'
 #' @docType data
 #'
---
+++ R/interactions.gni.R
@@ -0,0 +1,18 @@
+#' Pairs between Beaf-32 Peaks and TSS on 2L and 2R chromosomes.
+#'
+#' Data from a ChIp Seq experiment
+#'
+#' @docType data
+#'
+#' @usage data(interactions.gni)
+#'
+#' @format An object of class GInteractions.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(interactions.gni)
+#' interactions.gni
+"interactions.gni"
