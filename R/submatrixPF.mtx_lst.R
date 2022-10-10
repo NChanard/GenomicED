c3f85c8a46f0d4ae31579310697da8acd7a1c5d1
NChanard <nchanard@univ-tlse3.fr>
2022-10-10 11:11:42 +0200

    Add submatrixRF.mtx_lst

---
+++ R/submatrixPF.mtx_lst.R
@@ -0,0 +1,16 @@
+#' Drosophila Melanogaster extracted pairs between Beaf-32 Peaks and TSS on 2L and 2R chromosomes.
+#'
+#' @docType data
+#'
+#' @usage data(submatrixPF.mtx_lst)
+#'
+#' @format A list of object of matrix.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(submatrixPF.mtx_lst)
+#' submatrixPF.mtx_lst
+"submatrixPF.mtx_lst"
---
+++ R/submatrixRF.mtx_lst.R
@@ -0,0 +1,16 @@
+#' Drosophila Melanogaster extracted pairs between Beaf-32 Peaks and TSS on 2L and 2R chromosomes.
+#'
+#' @docType data
+#'
+#' @usage data(submatrixRF.mtx_lst)
+#'
+#' @format A list of object of matrix.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(submatrixRF.mtx_lst)
+#' submatrixRF.mtx_lst
+"submatrixRF.mtx_lst"
