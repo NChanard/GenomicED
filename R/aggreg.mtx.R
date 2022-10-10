54dcb02e833c0febde881b5d0b4af3df9b9442cf
NChanard <nchanard@univ-tlse3.fr>
2022-10-10 13:39:27 +0200

    Add aggregated.mtx

---
+++ R/aggreg.mtx.R
@@ -0,0 +1,16 @@
+#' Drosophila Melanogaster differential agggregated pairs between Beaf-32 Peaks and TSS on 2L and 2R chromosomes. Pairs extracted as regions feature.
+#'
+#' @docType data
+#'
+#' @usage data(aggreg.mtx)
+#'
+#' @format A list of object of matrix.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(aggreg.mtx)
+#' aggreg.mtx
+"aggreg.mtx"
