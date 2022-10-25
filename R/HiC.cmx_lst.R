7029d2761d8609c41f7eea78e04ce833691969ad
NChanard <nchanard@univ-tlse3.fr>
2022-10-25 13:17:26 +0200

    Add HiC.cmx_lst

---
+++ R/HiC.cmx_lst.R
@@ -0,0 +1,18 @@
+#' Drosophila Melanogaster HiC data on chromosome 2R and 2L in Beaf 32 depletion.
+#'
+#' Data from a HiC Seq experiment
+#'
+#' @docType data
+#'
+#' @usage data(HiC.cmx_lst)
+#'
+#' @format A a list of ContactMatrix objects. Each element correspond to the interaction matrix of two chromosomes.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(HiC.cmx_lst)
+#' HiC.cmx_lst
+"HiC.cmx_lst"
